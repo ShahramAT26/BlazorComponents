7981e67d76870b1e974c846c234414baf64ea774
Shahram <Shahram@DESKTOP-2M8KIAP>
2024-01-12 18:25:33 +0330

    Aop Progress bar

---
+++ BlazorTest/Infrastucture/AOP/Progress/Progress.cs
@@ -0,0 +1,34 @@
+﻿using MudBlazor;
+using Rougamo;
+using Rougamo.Context;
+
+namespace BlazorTest.Infrastucture.AOP.Progress
+{
+    public class Progress : MoAttribute
+    {
+        private int _onExitTimeoutDelay;
+
+        public Progress(int onExitTimeoutDelay = 0)
+        {
+            _onExitTimeoutDelay = onExitTimeoutDelay;
+        }
+
+        public override void OnEntry(MethodContext context)
+        {
+            ProgressComponentProvider.Show();
+        }
+
+        public override async void OnExit(MethodContext context)
+        {
+            try
+            {
+                await Task.Delay(_onExitTimeoutDelay);
+                ProgressComponentProvider.Hide();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception In Progress Attribute: {ex}");
+            }
+        }
+    }
+}
---
+++ BlazorTest/Infrastucture/AOP/Progress/ProgressComponentProvider.cs
@@ -0,0 +1,15 @@
+﻿using MudBlazor;
+
+namespace BlazorTest.Infrastucture.AOP.Progress
+{
+    public class ProgressComponentProvider
+    {
+        public static event Action OnShow;
+
+        public static event Action OnHide;
+
+        public static void Show() => OnShow?.Invoke();
+
+        public static void Hide() => OnHide?.Invoke();
+    }
+}
